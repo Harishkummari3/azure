719db6bfadf91c21f91e1b80d3ece2a620c28659
Harishkummari3 <103017636+Harishkummari3@users.noreply.github.com>
2022-05-03 10:48:30 +0530

    Adding files to github

---
+++ WelcomeWorldApplication.java
@@ -0,0 +1,13 @@
+package com.hari.demo.welcomeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeWorldApplication.class, args);
+	}
+
+}
